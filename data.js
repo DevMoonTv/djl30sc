1aafd551c13b21dc52a02487e2a3f23491377285
Glitch (agate-burnt-flood) <none>
2021-10-08 12:23:02 +0000

    Import of DevMoonTv/djl30tb

---
+++ data.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    title:
+      "OH Mãe como a Isabella ta no Baile ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "OH-Mãe-como-a-Isabella-ta-no-Baile.mp3"
+  },
+  {
+    title: "DJ ERY 2 - O MAESTRO DOS FLUXOS 2 ( Dj L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "O-MAESTRO-DOS-FLUXOS-2.mp3"
+  },
+  {
+    title: "AI AI BOTA BOTA BOTA - VEM NA VARA ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "AI-AI-BOTA-BOTA-BOTA-VEM-NA-VARA.mp3"
+  },
+  {
+    title: "DJ Ery - Montagem dos Relíquias Furacão 2000 ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "Montagem-dos-Relíquias-Furacão-2000.mp3"
+  },
+  {
+    title: "VEM PRO BAILE DO BEGA ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "VEM-PRO-BAILE-DO-BEGA.mp3"
+  },
+  {
+    title: "OLHA SO PIRANHA OQUE EU VOU TI DIZER  - SENTA NA MINHA PIKA 123 ( DJ L30 SC )",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "SENTA-NA-MINHA-PIKA-123.mp3"
+  }
+];
