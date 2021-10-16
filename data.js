c5b877eb96675a83a9553242c6af0bfd2d082a3a
DevMoonTv <75825236+DevMoonTv@users.noreply.github.com>
2021-10-16 18:42:56 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,27 @@
 export default [
   {
     title:
+      "ELA MAMA MAMA, MAMA - MC BIANO DO IMPÉRA + DJ L30 SC ",
+    artist: "Dj L30 SC",
+    cover: "ELA MAMA MAMA, MAMA - MC BIANO DO IMPÉRA + DJ L30 SC.png",
+    file: "ELA MAMA MAMA, MAMA - MC BIANO DO IMPÉRA + DJ L30 SC.mp3"
+  },
+  {
+    title:
+      "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "GP DA ZL, MC Vitinho Avassalador - Automotivo Embrazante.mp3"
+  },
+  {
+    title:
+      "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave ( DJ L30 SC ) ",
+    artist: "Dj L30 SC",
+    cover: "capa-l30sc.png",
+    file: "DJ Shalom, mc jhenny, MC Yuri, MC Marsha, MC P1, GP DA ZL - Fds Tô Na Rave.mp3"
+  },
+  {
+    title:
       "OH Mãe como a Isabella ta no Baile ( DJ L30 SC ) ",
     artist: "Dj L30 SC",
     cover: "capa-l30sc.png",
