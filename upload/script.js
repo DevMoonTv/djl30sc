a972e7dfe0c0ee7531eec1a39562bdad16bfe709
DevMoonTv <75825236+DevMoonTv@users.noreply.github.com>
2021-10-18 23:48:50 -0300

    Create script.js

---
+++ upload/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('flImage');
+
+photo.addEventListener('click', () => {
+    file.click();
+});'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('flImage');
+
+photo.addEventListener('click', () => {
+    file.click();
+});'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('flImage');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
